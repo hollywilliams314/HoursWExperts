9a4222c7541bb6f0fbd1f6cf3bfdf553f09289f8
Holly <williamsmom5@yahoo.com>
2018-03-21 19:53:44 -0500

    fixed reverse array in place

--- ch4/reverseArray.js
+++ ch4/reverseArray.js
@@ -10,13 +10,20 @@ function reverseArray(inArray) {
 function reverseArrayInPlace(arrayValue) {
 
     let arraySize = arrayValue.length;
-    let revArray = reverseArray(arrayValue);
-
-    for (let counter = 0; counter < arraySize; counter++) {
-      arrayValue[counter] = revArray[counter];
+    // let revArray = reverseArray(arrayValue);
+    //
+    // for (let counter = 0; counter < arraySize; counter++) {
+    //   arrayValue[counter] = revArray[counter];
+    // }
+    let j = 1;
+    halfArraySize = Math.trunc(arraySize/2);
+    for (i = 0; i < arraySize - 1; i++) {
+      let temp = arrayValue[i];
+      arrayValue[i] = arrayValue[arraySize - j];
+      arrayValue[arraySize - j] = temp;
+      j++;
     }
 
-  //  arrayValue = revArray;    nope
 }
 
 console.log(reverseArray(["A", "B", "C"]));

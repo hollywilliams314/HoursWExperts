989300ccddc7597b83195f794a5b1b9c9c9f180a
Holly <williamsmom5@yahoo.com>
2018-03-21 19:35:24 -0500

    recursive deepcompr and better deepcomp

--- ch4/DeepComp.js
+++ ch4/DeepComp.js
@@ -14,9 +14,6 @@ function deepEqual(val1,val2) {
   }
   else {  // objects must have same properties and values
     if(JSON.stringify(val1) == JSON.stringify(val2)) {
-      // console.log("inside stringify ==  block");
-      // console.log(JSON.stringify(val1));
-      // console.log(JSON.stringify(val2));
       return true;
     }
     else {
@@ -25,19 +22,14 @@ function deepEqual(val1,val2) {
 
   } // end else we have objects
 
-//x == "object" && x != null;
-
 }
 
 let obj = {here: {is: "an"}, object: 2};
-console.log("actual test 1: sb true");
+
 console.log(deepEqual(obj, obj));
-//console.log("stringify: " + JSON.stringify(obj));
-//console.log("typeof stringify: " + typeof(JSON.stringify(obj)));
-// → true
-console.log("actual test 2: sb false");
+
 console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
-console.log("actual test 3: sb true");
+
 console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
